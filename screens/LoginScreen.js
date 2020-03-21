fef615ec64ec57d97985a9cf76fc3b4af090660e
Hal Peters <geohalbert@gmail.com>
2020-03-20 23:05:37 -0500

    app navigation added, dependencies, bare screens

---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import styles from "../styles/screens/LoginScreenStyles";
+
+export default function LoginScreen({}) {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
