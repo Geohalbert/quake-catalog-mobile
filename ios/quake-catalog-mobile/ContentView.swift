ff32e35f64a0ec6b453e3d44f92d00ea9050a193
Hal Peters <geohalbert@gmail.com>
2020-03-20 19:14:52 -0500

    setup ios and firebase

---
+++ ios/quake-catalog-mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quake-catalog-mobile
+//
+//  Created by Hal Peters on 3/20/20.
+//  Copyright © 2020 Hal Peters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
